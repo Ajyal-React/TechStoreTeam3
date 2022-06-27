fdfe62fb8114be5ffd4b51daa0bb459e43b45065
unknown <you@example.com>
2022-06-28 00:19:02 +0300

    Adding media in trending section

--- src/components/Trending/TrendingCards.jsx
+++ src/components/Trending/TrendingCards.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { SmTitle, LgTitle } from "../../GlobalStyles";
-import { ContinerCard, ContinerItem } from "../Trending/Trending.styled";
+import { ContinerCard, ContinerItem , SmTitle, LgTitle} from "../Trending/Trending.styled";
 import Trending from "./Trending";
 import { getTrending } from "../../redux/actions/trendingAction";
 import Spinner from "../Spinner/Spinner";
